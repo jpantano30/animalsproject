1e433043641a5b287c5d0f6788b02a07044b4745
Jena <jpantano30@gmail.com>
2024-06-13 17:00:52 -0400

    errors

--- birds/serializers.py
+++ birds/serializers.py
@@ -2,7 +2,7 @@ from .models import Birds
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-class BirdSerializer(serializers.HyperlinkModelSerializer):
+class BirdSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Birds
     fields = ['id', 'subject', 'details']
--- cats/serializers.py
+++ cats/serializers.py
@@ -2,7 +2,7 @@ from .models import Cats
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-class CatSerializer(serializers.HyperlinkModelSerializer):
+class CatSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Cats
     fields = ['id', 'subject', 'details']
